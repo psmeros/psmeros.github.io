18bb90a7786277e8ba9b307ecb4d4cd3f65d2d69
Panayiotis Smeros <psmeros@di.uoa.gr>
2015-04-24 16:49:00 +0300

    Automatically update date.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sed -i "s|?DATE?|`date +%d/%m/%Y`|g" public_html/index.html
 mkdir tmp
 sudo sshfs psmeros@users.uoa.gr: tmp
 sudo rm -r tmp/public_html
 sudo cp -r public_html tmp/
 sudo umount tmp
 rm -r tmp
+sed -i "s|`date +%d/%m/%Y`|?DATE?|g" public_html/index.html
 
