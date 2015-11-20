76ee01341580d12f6e9a979814e6ba637b04e816
Panayiotis Smeros <psmeros@di.uoa.gr>
2015-11-20 04:13:25 +0000

    Read publications from csv.

--- publish.sh
+++ publish.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
+#Requires SQLite and sshfs.
 
-sed -i "s|?DATE?|`date +%d/%m/%Y`|g" public_html/index.html
-mkdir tmp
+sqlite3 publicationDB ".mode csv"
+sqlite3 publicationDB ".separator \"|\""
+sqlite3 publicationDB ".import etc/publications.csv publications"
+sqlite3 publicationDB ".import etc/categories.csv categories"
+sqlite3 publicationDB ".mode tabs"
+
+publicationFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Conference || ' ' || PlaceTime || ' ' || Link || '</p></li>'"
+categoryFormat="'<a id=\"' || replace(Category,\" \",\"\") || '\" href=\"#' || replace(Category,\" \",\"\") || '\">' || Category || '</a>'"
+
+publications=`sqlite3 publicationDB "select '<p>' || $categoryFormat || '<br/>' || group_concat(publicationlist, ' ') || '</p>' from (
+              select Category, CategoryID, Year, '<b>' || Year || '</b>' || '<ul>' || group_concat($publicationFormat,' ') || '</ul>' as publicationlist from publications, categories 
+              WHERE ID=CategoryID group by CategoryID, Year order by Year DESC, Month DESC) tmp group by CategoryID order by CategoryID"`
+
+publications=`echo "$publications" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
+
+rm publicationDB
+
+
+sudo mkdir tmp
 sudo sshfs psmeros@users.uoa.gr: tmp
 sudo rm -r tmp/public_html
 sudo cp -r public_html tmp/
+sudo sed -i "s|?PUBLICATIONS?|`echo $publications`|g" tmp/public_html/index.html
+sudo sed -i "s|?DATE?|`date +%d/%m/%Y`|g" tmp/public_html/index.html
 sudo umount tmp
-rm -r tmp
-sed -i "s|`date +%d/%m/%Y`|?DATE?|g" public_html/index.html
+sudo rm -r tmp
+
 
