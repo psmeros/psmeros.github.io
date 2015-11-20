b4a621143fbe45e3c2625838e8083bbce33df678
Panayiotis Smeros <psmeros@di.uoa.gr>
2015-11-20 13:27:57 +0000

    Read deliverables from csv.

--- publish.sh
+++ publish.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 #Requires SQLite and sshfs.
 
-sqlite3 publicationDB ".mode csv"
-sqlite3 publicationDB ".separator \"|\""
-sqlite3 publicationDB ".import etc/publications.csv publications"
-sqlite3 publicationDB ".import etc/categories.csv categories"
-sqlite3 publicationDB ".mode tabs"
+sqlite3 publicationsDB ".mode csv"
+sqlite3 publicationsDB ".separator \"|\""
+sqlite3 publicationsDB ".import etc/publications.csv publications"
+sqlite3 publicationsDB ".import etc/categories.csv categories"
+sqlite3 publicationsDB ".mode tabs"
 
-publicationFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Conference || ' ' || PlaceTime || ' ' || Link || '</p></li>'"
+publicationFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Conference || ' ' || PlaceTime || ' ' || case when like('%.pdf', Link)=1 then '[<a href=\"' || Link || '\">pdf</a>]' else case when like('http%', Link)=1 then '[<a href=\"' || Link || '\">link</a>]' else '' end end || '</p></li>'"
 categoryFormat="'<a id=\"' || replace(Category,\" \",\"\") || '\" href=\"#' || replace(Category,\" \",\"\") || '\">' || Category || '</a>'"
 
-publications=`sqlite3 publicationDB "select '<p>' || $categoryFormat || '<br/>' || group_concat(publicationlist, ' ') || '</p>' from (
+publications=`sqlite3 publicationsDB "select '<p>' || $categoryFormat || '<br/>' || group_concat(publicationlist, ' ') || '</p>' from (
               select Category, CategoryID, Year, '<b>' || Year || '</b>' || '<ul>' || group_concat($publicationFormat,' ') || '</ul>' as publicationlist from publications, categories 
               WHERE ID=CategoryID group by CategoryID, Year order by Year DESC, Month DESC) tmp group by CategoryID order by CategoryID"`
 
 publications=`echo "$publications" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
 
-rm publicationDB
+rm publicationsDB
+
+
+sqlite3 deliverablesDB ".mode csv"
+sqlite3 deliverablesDB ".separator \"|\""
+sqlite3 deliverablesDB ".import etc/deliverables.csv deliverables"
+sqlite3 deliverablesDB ".mode tabs"
+
+deliverableFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Info || '</p></li>'"
+
+deliverables=`sqlite3 deliverablesDB "select '<ul>' || $deliverableFormat || '</ul>' from deliverables
+              order by Year DESC, Priority"`
+
+deliverables=`echo "$deliverables" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
+
+rm deliverablesDB
 
 
 sudo mkdir tmp
@@ -24,6 +39,7 @@ sudo sshfs psmeros@users.uoa.gr: tmp
 sudo rm -r tmp/public_html
 sudo cp -r public_html tmp/
 sudo sed -i "s|?PUBLICATIONS?|`echo $publications`|g" tmp/public_html/index.html
+sudo sed -i "s|?DELIVERABLES?|`echo $deliverables`|g" tmp/public_html/index.html
 sudo sed -i "s|?DATE?|`date +%d/%m/%Y`|g" tmp/public_html/index.html
 sudo umount tmp
 sudo rm -r tmp
