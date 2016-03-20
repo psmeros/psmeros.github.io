24868c03e2b431c8e83881302fad9bdbd31e3914
Panayiotis Smeros <psmeros@di.uoa.gr>
2016-03-20 14:15:47 +0100

    Website live @ panayiotis.smeros.info.

--- website/publish.sh
+++ website/publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ../etc/addContentWebsite.sh
-scp -r public_html psmeros@jose.di.uoa.gr:
-ssh -t psmeros@jose.di.uoa.gr 'mkdir tmp; sshfs psmeros@users.uoa.gr: tmp; rm -r tmp/public_html; cp -r public_html tmp/; fusermount -u tmp; rm -r tmp public_html'
+mkdir papaki
+curlftpfs psmeros:TerastioPassword\!11@5.9.111.116 papaki
+rm -r papaki/panayiotis.smeros.info/*
+cp -r public_html/* papaki/panayiotis.smeros.info/
+sudo fusermount -u papaki
+rm -r papaki
 ../etc/removeContentWebsite.sh
