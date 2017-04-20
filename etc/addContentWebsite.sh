fd43dcb655aa9e096d54fe5e480c3a4fdf5c3481
Panayiotis Smeros <panayiotis.smeros@epfl.ch>
2017-04-20 19:01:20 +0200

    Kept only selected publications in the front page and added 'slides' attribute.

--- etc/addContentWebsite.sh
+++ etc/addContentWebsite.sh
@@ -14,17 +14,20 @@ publications=`sqlite3 ../etc/psmerosDB "select '<p>' || $categoryFormat || '<br/
 publications=`echo "$publications" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
 
 
-deliverableFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Info || '</p></li>'"
+selectedPublicationFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Conference || ' ' || PlaceTime || ' ' || case when like('%.pdf', Link)=1 then '[<a href=\"publications/' || Link || '\">pdf</a>]' else case when like('http%', Link)=1 then '[<a href=\"' || Link || '\">link</a>]' else '' end end || case when like('%.pdf', Slides)=1 then ' [<a href=\"publications/' || Slides || '\">slides</a>]' else '' end || '</p></li>'"
 
-deliverables=`sqlite3 ../etc/psmerosDB "select '<ul>' || $deliverableFormat || '</ul>' from deliverables
-              order by Year DESC, Priority"`
-
-deliverables=`echo "$deliverables" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
+selectedPublications=`sqlite3 ../etc/psmerosDB "select '<ul>' || $selectedPublicationFormat || '</ul>' from publications WHERE Selected=1 order by Year DESC, Month DESC"`
+selectedPublications=`echo "$selectedPublications" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
 
 
-
-sed -i "s|?PUBLICATIONS?|`echo $publications`|g" "$folder"/index.html
-sed -i "s|?DELIVERABLES?|`echo $deliverables`|g" "$folder"/index.html
+sed -i "s|?PUBLICATIONS?|`echo $publications`|g" "$folder"/allPublications.html
+sed -i "s|?PUBLICATIONS?|`echo $selectedPublications`|g" "$folder"/index.html
 sed -i "s|?DATE?|`date +%d/%m/%Y`|g" "$folder"/index.html
 
 
+#List Deliverables (deprecated)
+#deliverableFormat="'<li><p>' || Authors || ' ' || '<i>' || Title || '</i>' || ' ' || Info || '</p></li>'"
+#deliverables=`sqlite3 ../etc/psmerosDB "select '<ul>' || $deliverableFormat || '</ul>' from deliverables
+#              order by Year DESC, Priority"`
+#deliverables=`echo "$deliverables" | sed 's/P. Smeros/\<b\>P. Smeros\<\/b\>/g'`
+#sed -i "s|?DELIVERABLES?|`echo $deliverables`|g" "$folder"/index.html
